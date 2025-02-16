efcbd5318cc2757db7abb9c3a5ad76669380540b
ivan <ivansangomez6@gmail.com>
2025-02-16 12:36:24 -0500

    deleted images

--- src/components/FrontSkills.tsx
+++ src/components/FrontSkills.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+
 import "./FrontSkills.css"
 const FrontSkills = () => {
   return (
--- src/components/ui/HoverButton.tsx
+++ src/components/ui/HoverButton.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+
 import "./HoverButton.css"
 
 type HoverLink = {
